3fa77bfa845ba4a61eff1111e42fe7899a027a4e
dilipp07 <dilipkupanda23@gmail.com>
2023-08-27 22:20:44 +0530

    training pipeline done

--- src/components/data_injestion.py
+++ src/components/data_injestion.py
@@ -5,8 +5,7 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-from src.components.data_transformation import DataTransformation
-from src.components.model_trainer import ModelTrainer
+
 
 
 
@@ -52,12 +51,3 @@ class DataInjestion:
             raise CustomException(e,sys)
             
 
-if __name__=="__main__":
-
-    obj=DataInjestion()
-
-    train_data_path,test_data_path=obj.initate_data_injestion()
-    data_transformation=DataTransformation()
-    train_arr,test_arr,_=data_transformation.initaite_data_transformation(train_data_path,test_data_path)
-    model_trainer=ModelTrainer()
-    model_trainer.initiate_model_trainer(train_arr,test_arr)
